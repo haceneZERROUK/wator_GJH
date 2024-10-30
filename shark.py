a6268462b7bf54ef9c6daaaa3f43dc2d95370dc5
hacene.zerrouk@gmail.com <hacene.zerrouk@gmail.com>
2024-10-30 12:26:15 +0100

    création class shark et fish

--- shark.py
+++ shark.py
@@ -1,16 +1,11 @@
-VALEUR_INITIALE_REPRODUCTION = 5
+from fish import Fish
 
 
 class Shark(Fish):
-    def __init__(self, position, indice_reproduction, energy_value, vivant):
-        super().__init__(position, indice_reproduction)
-        self.energy_value = energy_value
-        self.vivant = vivant
+    def __init__(self, position : tuple, indice_reproduction : int, current_reproduction = 0, chronon = 0, energy  = 10):
+        super().__init__(position, indice_reproduction, current_reproduction, chronon)
+        self.energy = energy
 
-    def reproduction(self):
-        if self.indice_reproduction >= VALEUR_INITIALE_REPRODUCTION:
-            if Shark.scan(self.position)
-
-    def maj_reproduction(self):
-        self.indice_reproduction +=1
-        return self.indice_reproduction
+        def eat(self):
+            self.energy = 10
+            
