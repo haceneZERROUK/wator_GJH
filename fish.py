7502a7f7398ce57128458b2e8ca7b4bdb7bb6012
hacene.zerrouk@gmail.com <hacene.zerrouk@gmail.com>
2024-10-30 14:09:59 +0100

    shark et fish avec constante

--- fish.py
+++ fish.py
@@ -1,25 +1,23 @@
+
 class Fish:
-    def __init__(self, position : tuple, indice_reproduction : int, current_reproduction = 0, chronon = 0 ):
+    def __init__(self, position : tuple, valeur_accouchement = VALEUR_ACCOUCHEMENT_POISSON, chronon = 0 ):
         self.position = position
-        self.current_reproduction = current_reproduction
-        self.indice_reproduction = indice_reproduction
+        self.valeur_accouchement = valeur_accouchement
+        self.indice_reproduction = 0
         self.chronon = chronon
 
 
     def incrementation_chronon(self):
         self.chronon += 1
 
-    def augmentation_current_reproduction(self):
-        self.current_reproduction += 1
+    def incrementation_indice_reproduction(self):
+        self.indice_reproduction += 1
 
-    def reset_current_reproduction(self):
-        self.current_reproduction = 0
+    def reset_indice_reproduction(self):
+        self.indice_reproduction = 0
 
     def set_value(self, x,y):
         self.position = (x,y)
     
     def get_value(self):
         return self.position
-
-        
-
--- shark.py
+++ shark.py
@@ -1,14 +1,19 @@
 from fish import Fish
 
 
+
 class Shark(Fish):
-    def __init__(self, position : tuple, indice_reproduction : int, current_reproduction = 0, chronon = 0, energy  = 10):
-        super().__init__(position, indice_reproduction, current_reproduction, chronon)
+    def __init__(self, position : tuple, valeur_accouchement = VALEUR_CHOISIE_ACCOUCHEMENT_REQUIN, chronon = 0, energy  = 10):
+        super().__init__(position, valeur_accouchement, chronon)
+
         self.energy = energy
+        self.valeur_accouchement = valeur_accouchement
+        self.indice_reproduction = 0
 
         def eat(self):
             self.energy = 10
 
         def perte_energy(self):
-            self.energy += 1
+            self.energy -= 1
+
 
